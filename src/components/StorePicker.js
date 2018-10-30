2555b2d3d8cc9e7ddb0ba57738af1436f73d3b1d
sandond77 <sandu@ucdavis.edu>
2018-10-30 15:01:56 -0700

    added react-dom to index.js and created StorePicker.js

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class StorePicker extends Component {
+	render() {
+		return (
+			<div>
+				<p> I am the store picker </p>
+			</div>
+		);
+	}
+}
