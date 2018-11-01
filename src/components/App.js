3cf6b0db56bb2d4322055c3080ba2562990e4324
sandond77 <sandu@ucdavis.edu>
2018-11-01 15:14:08 -0700

    finished Header.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
 	render() {
@@ -7,8 +8,8 @@ class App extends Component {
 				<div className="menu">	
 					<Header />
 				</div>
-				<Inventory />
-				<Order />
+				{/* <Inventory />
+				<Order /> */}
 			</div>
 		);
 	}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render() {
 		return (
-			<div>
-				
-			</div>
+			<header className="top">
+				<h1>Catch 
+					<span className="ofThe">
+						<span className="of">Of</span>
+						<span className="the">The</span>
+					</span>
+					Day
+				</h1>
+				<h3 className="tagline">
+					<span>Fresh Seafood Market</span>
+				</h3>
+			</header>
 		);
 	}
 }
