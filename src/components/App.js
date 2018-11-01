b3430ac6f72d6c6908b81adb8080a58b6016da57
sandond77 <sandu@ucdavis.edu>
2018-11-01 14:40:55 -0700

    imported style.css into index.js page

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="Catch-Of-The-Day">
+					
+			</div>
+		);
+	}
+}
+
+export default App;
