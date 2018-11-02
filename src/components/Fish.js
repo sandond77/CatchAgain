73eb03bcb111b14ae7b296e241052a7a14d89a5a
sandond77 <sandu@ucdavis.edu>
2018-11-02 11:09:51 -0700

    finished most of Fish.js - can now render out fish data

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends Component {
 	render() {
+		const { image, name, price, desc, status} = this.props.details
 		return (
-			<div className="single-fish">
-				🐟🐟
-			</div>
+			<li className='menu-fish'>
+				<img src={image} alt={name} />
+				<h3 className='fish-name'>
+					{name}<span>{formatPrice(price)}</span>
+				</h3>
+				<p>{desc}</p>
+				<button>Add to Cart</button>
+			</li>
 		);
 	}
 }
