4513854519bcd116590e140b5644cd85a7872faa
sandond77 <sandu@ucdavis.edu>
2018-11-01 21:23:11 -0700

    working on event handler for storepicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
-export default class StorePicker extends Component {
+class StorePicker extends Component {
+	myInput = React.createRef();
+
+	goToStore = event => {
+		event.preventDefault();
+		console.log(this.myInput.current.value)
+	};
+	
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input 
+					type="text" 
+					required 
+					placeholder="Store Name" 
+					ref={this.myInput} 
+					defaultValue={ getFunName() }
+				/>
 				<button type="submit"> Visit Store ➡️ </button>
 			</form>
 		);
 	}
 }
+
+export default StorePicker;
