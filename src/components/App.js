b667aed78c76ad859110c70b5fda9669a20c3522
sandond77 <sandu@ucdavis.edu>
2018-11-02 11:03:00 -0700

    successfully iterated through state object to render out fish components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
 
 class App extends Component {
@@ -30,7 +31,7 @@ class App extends Component {
 				<div className="menu">	
 					<Header tagline="Fresh Seafood Market"/>
 					<ul className="fishes">
-
+						{Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
 					</ul>
 				</div>
 				<Order />
