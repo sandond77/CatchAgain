a58d3c12741084c53884af091f78a0298cc0e6f2
sandond77 <sandu@ucdavis.edu>
2018-11-07 15:16:45 -0800

    working on css transitions

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Order extends Component {
 	renderOrder = (key) => {
@@ -35,9 +36,9 @@ class Order extends Component {
 		return (
 			<div className='order-wrap'>
 				<h2>Order</h2>
-				<ul className='order'>
+				<TransitionGroup component='ul' className='order'>
 					{orderIds.map(this.renderOrder)}
-				</ul>
+				</TransitionGroup>
 				<div className="total">
 					Total:
 					<strong>{formatPrice(total)}</strong>
